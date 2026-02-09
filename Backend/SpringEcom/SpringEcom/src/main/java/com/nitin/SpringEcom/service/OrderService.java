5ad739313a4cc92dbd72f29d451dcddac84b0c1d
NitinTiwari56 <nitin200409@gmail.com>
2026-02-09 21:29:21 +0530

    Add files via upload

---
+++ Backend/SpringEcom/SpringEcom/src/main/java/com/nitin/SpringEcom/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.nitin.SpringEcom.service;
+
+import com.nitin.SpringEcom.model.dto.OrderRequest;
+import com.nitin.SpringEcom.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
+        return null;
+    }
+}
